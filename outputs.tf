964de9fc64018cb11592aa2cd3f94bec1e2b6552
Bala Senapathi <57207778+balusena@users.noreply.github.com>
2025-11-25 23:31:36 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "file_path" {
-  value = local_file.example.filename
+  value       = local_file.example.filename
+  description = "Path to the generated local file"
 }
 
 output "location_used" {
-  value = var.location
+  value       = var.location
+  description = "Location used in the example file"
 }
 
 output "project_used" {
-  value = var.project
+  value       = var.project
+  description = "Project name used in the example file"
 }
